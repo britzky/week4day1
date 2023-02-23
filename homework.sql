38602b7cebe7be0971e231da0263a1841fdd5922
John <johnclapper89@gmail.com>
2023-02-22 20:40:05 -0500

    fixed mistakes in number 2, 7 and 9

--- homework.sql
+++ homework.sql
@@ -6,7 +6,7 @@ WHERE last_name = 'Wahlberg';
 -- Answer: 2
 
 -- 2. How many payments were made between $3.99 and $5.99?
-SELECT amount
+SELECT COUNT(amount)
 FROM payment
 WHERE amount BETWEEN 3.99 AND 5.99;
 
@@ -54,10 +54,11 @@ FROM customer;
 -- Answer: 733 distinct names
 
 -- 7. What film has the most actors in it? (use film_actor table and get film_id)
-SELECT actor_id, COUNT(film_id)
+SELECT film_id, COUNT(actor_id)
 FROM film_actor
-GROUP BY actor_id;
--- Answer: actor_id 58 has 32 film_id's
+GROUP BY film_id
+ORDER BY COUNT(actor_id) DESC;
+-- Answer: film 508 has the most actors with 15
 
 -- 8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
 SELECT last_name
@@ -68,12 +69,12 @@ WHERE last_name LIKE '%es' AND store_id <> 2;
 
 -- 9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
 -- with ids between 380 and 430? (use group by and having > 250)
-SELECT amount, customer_id
+SELECT COUNT(amount), amount
 FROM payment
 WHERE customer_id BETWEEN 380 AND 430
-GROUP BY amount, customer_id
-HAVING amount > 250;
---Answer: Still working on this one. I keep getting no data
+GROUP BY amount
+HAVING COUNT(amount) > 250;
+--Answer: 3 
 
 -- 10. Within the film table, how many rating categories are there? And what rating has the most
 -- movies total?
